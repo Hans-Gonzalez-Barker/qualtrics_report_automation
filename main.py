3e3e0b50dd7c8bc68982149f3ef288b42ed82f5c
Hans-Gonzalez-Barker <hansgonzalez1216@gmail.com>
2026-04-06 12:13:22 -0400

    feat: process for loading in and cleaning the CSV for visualization in main.py.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import time
+import glob
+import pandas as pd
 from dotenv import load_dotenv
 from api_handler import qualtrics_export, check_export_status, download_qualtrics_file
+from data_processing import load_and_clean_data, get_workshop_metrics
 
 load_dotenv()
 
 
 def main():
 
+    # --- DATA ACQUISITION ---
     # Begin data exportation from Qualtrics
     progress_id = qualtrics_export()
     print(f"Export started: {progress_id}")
@@ -39,6 +43,32 @@ def main():
 
     # Download and Unzip data
     download_qualtrics_file(file_id)
+
+
+
+    # --- DATA PROCESSING ---
+    # Get the CSV
+    csv_files = glob.glob("data/*.csv")
+    if not csv_files:
+        # For testing purposes
+        print("No CSV found in data folder.")
+        return
+
+    raw_csv = csv_files[0]
+    df = load_and_clean_data(raw_csv)
+
+    # Get list of unique workshops
+    workshops = df['Workshop'].unique()
+
+    for workshop in workshops:
+        if pd.isna(workshop): continue
+
+        print(f"Processing metrics for: {workshop}")
+
+        # Calculate scores and grab comments for this specific workshop
+        averages, enjoy_text, change_text = get_workshop_metrics(df, workshop)
+
+    print("All data processed. Visualizing...")
 
 
 if __name__ == "__main__":
